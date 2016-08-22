1ac264acf55c8c1ef32a4761878b06d442f8ab3b
atonizer <atonizer@gmail.com>
2016-08-22 14:23:18 -0400

    E-commerce website project

---
+++ template/_footer.php
@@ -0,0 +1,12 @@
+
+</div>
+<div class="cl">&#160;</div>
+</div>
+</div>
+<div id="footer">
+    <div id="footer_in">
+        &copy; <a href="http://site.loc" title="E-commerce Website" target="_blank">E-commerce Website</a>
+    </div>
+</div>
+</body>
+</html>
